65ab033efd3e965158f741514b100076d696fd47
devadev2005 <145647439+devadev2005@users.noreply.github.com>
2025-10-18 19:33:23 +0530

    Add files via upload
    blood donation .py

---
+++ blood donation.py
@@ -0,0 +1,13 @@
+age=int(input("enter the age :")
+weight=int(inptut("enter the weight:")
+if age <=18 and age >=60:
+     if weight>=50:
+           print("you are eliglible for blood donation")
+           else:
+               print("your weight is not 50")
+else:
+    print("your is is not 18")
+                     
+           
+           
+        
