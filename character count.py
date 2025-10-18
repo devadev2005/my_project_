1a81e65fa037fb59f7f4122a3531dd3779b8f943
devadev2005 <145647439+devadev2005@users.noreply.github.com>
2025-10-18 19:34:35 +0530

    character count

---
+++ character count.py
@@ -0,0 +1,16 @@
+n=input()
+count={}
+
+for i in n:
+    if i in count:
+        count[i]+=1
+    else:
+        count[i]=1
+print(count)
+printed=set()        
+for char ,frq in count.items():
+    if i not in printed:
+         print(f"{char}:{frq}")
+         printed.add(frq)
+        
+    
